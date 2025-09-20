e533544f2164a182d36f66b0fcdbe9991021953d
Saurav19956 <gsaurav760@gmail.com>
2025-09-20 11:21:17 +0530

    install sequilize orm

---
+++ backend/src/commons/dbConfig.ts
@@ -0,0 +1,12 @@
+import { config } from "dotenv";
+
+
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+    db_user:
+    db_name:
+    db_password:
+    db_port:
+    db_host:
+})
